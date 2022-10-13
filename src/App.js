9e14ad34ec1a3c3f429534ab82af85da27e8e14d
Imon <contact@imonweb.co.uk>
2022-10-13 21:01:59 +0100

    app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,9 @@
  
-import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-    
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      Hello World!
     </div>
   );
 }
