69263058fd65afdcccf25bfdd5dc10e2898dedd6
Imon <contact@imonweb.co.uk>
2022-11-28 23:39:31 +0000

    home

---
+++ src/App.js
@@ -0,0 +1,26 @@
+
+// import Directory from './components/directory/directory.component';
+import { Routes, Route } from 'react-router-dom';
+
+import Home from "./routes/home/home.component";
+
+const Shop = () => {
+  return (
+    <h1>I am the shop page.</h1>
+  )
+}
+
+// import './categories.styles.scss';
+
+const App = () => {
+  return (
+    <Routes>
+      <Route path='/home' element={<Home />}>
+        <Route path='shop' element={<Shop />} /> 
+        {/* /shop */}
+      </Route> 
+    </Routes>
+  );
+}
+
+export default App;
