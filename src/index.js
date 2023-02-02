94367c4f1ceb8647839650b73456f3ead2ad2d88
Imon <contact@imonweb.co.uk>
2023-02-02 18:14:32 +0000

    category item  component

--- src/index.js
+++ src/index.js
@@ -9,10 +9,11 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+  
       <App />
-    </BrowserRouter>
+     
   </React.StrictMode>
+  
 );
 
  
