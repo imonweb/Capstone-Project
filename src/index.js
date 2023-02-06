1641c53fce2df359c82a21a65e172489bc24d4cf
Imon <contact@imonweb.co.uk>
2023-02-06 22:29:00 +0000

    react router

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 
 import './index.scss';
 import App from './App';
@@ -9,9 +9,11 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-  
+    <BrowserRouter>
+    
       <App />
      
+    </BrowserRouter>
   </React.StrictMode>
   
 );
