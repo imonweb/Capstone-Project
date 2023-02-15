2a1f5402a05bb75a75033cff98279e825b5d2d5a
Imon <contact@imonweb.co.uk>
2023-02-15 23:45:33 +0000

    navigation

--- src/App.js
+++ src/App.js
@@ -1,16 +1,6 @@
 import { Routes, Route, Outlet } from 'react-router-dom'
 import Home from './routes/home/home.component';
-
-const Navigation = () => {
-  return (
-    <div>
-      <div>
-        <h1>I am the navigation bar</h1>
-      </div>
-      <Outlet />
-    </div>
-  )
-}
+import Navigation from './routes/navigation/navigation.component';
 
 const Shop = () => {
   return <h1>I am the shop page</h1>
