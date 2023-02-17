7418d70d24b991d9f71ba70bd1d86051bfdf9cfb
Imon <contact@imonweb.co.uk>
2023-02-17 23:52:19 +0000

    signin

---
+++ src/utils/firebase/firebase.utils.js
@@ -0,0 +1,31 @@
+import { initializeApp } from 'firebase/app'
+// import { getAuth, signInWithRedirect, signInWithPopup, GoogleAuthProvider} from 'firebase/auth'
+
+import {
+  getAuth,
+  signInWithRedirect,
+  signInWithPopup,
+  GoogleAuthProvider,
+} from 'firebase/auth';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAFxgUweDoYbG9GE2wh5NTefHlvrztl2s4",
+  authDomain: "crwn-clothing-db-60c8f.firebaseapp.com",
+  projectId: "crwn-clothing-db-60c8f",
+  storageBucket: "crwn-clothing-db-60c8f.appspot.com",
+  messagingSenderId: "279391310733",
+  appId: "1:279391310733:web:ac94c5065f1b44f89963c4"
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+
+const provider = new GoogleAuthProvider();
+
+provider.setCustomParameters({
+  prompt: "select_account"
+})
+
+export const auth = getAuth()
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider)
+
