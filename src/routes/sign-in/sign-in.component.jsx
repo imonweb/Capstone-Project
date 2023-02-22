34bbcf168fb2ad9e6a91e2131dffed9fbb15c142
Imon <contact@imonweb.co.uk>
2023-02-22 11:52:08 +0000

    creating user docs

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,19 +1,39 @@
 import React from 'react'
-import { signInWithGooglePopup, createUserDocumentFromAuth } from '../../utils/firebase/firebase.utils'
+import { useEffect } from 'react'
+import { getRedirectResult } from 'firebase/auth'
+
+import { 
+  auth,
+  signInWithGooglePopup, 
+  createUserDocumentFromAuth, 
+  signInWithGoogleRedirect 
+} from '../../utils/firebase/firebase.utils'
 
 const SignIn = () => {
 
+  useEffect( async () => {
+    const response = await getRedirectResult(auth)
+    console.log(response)
+  }, [])
+
   const logGoogleUser = async () => {
     const { user } = await signInWithGooglePopup()
-    const userDocRef = await createUserDocumentFromAuth(user)
-    // console.log(response) 
+   
+    console.log(user) 
   }
+
+  // const logGoogleRedirectUser = async () => {
+  //   const { user } = await signInWithGoogleRedirect()
+  //   const userDocRef = await createUserDocumentFromAuth(user)
+  // }
+
+
   return (
     <div>
       <h1>Sign In Page</h1>
-      <button onClick={logGoogleUser}>
-        Sign in with Google Popup
-      </button>
+      <button onClick={logGoogleUser}>Sign in with Google Popup</button>
+      {/* <button onClick={logGoogleRedirectUser}>Sign in with Google Redirect</button> */}
+      <button onClick={signInWithGoogleRedirect}>Sign in with Google Redirect</button>
     </div>
   )
 }
